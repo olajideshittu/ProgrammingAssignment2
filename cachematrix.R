4deef8fd2f68b1fba74c02b566d34bea59df4ea2
Andrew Duke <duke.a@husky.neu.edu>
2014-04-19 13:34:58 -0400

    Updated cachematrix.R for R Programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    xinv<- NULL
+    set<-function(y) {
+        x<<-matrix(y)
+        xinv<<- NULL
+    }
+    get <- function() x
+    setinv<- function(matinv) xinv<<-matinv
+    getinv<- function() xinv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    xinv <- x$getinv()
+    if (!is.null(xinv)) {
+        message("Getting cached data ...")
+        return(xinv)
+    }
+    data <-x$get()
+    xinv<-solve(data)
+    x$setinv(xinv)
+    xinv
 }

b62b28c9be8f03cf8002a5ecc635f7b8ed78ad0b
agupta83 <ashishgupta83@gmail.com>
2014-04-21 11:00:32 -0500

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Script to caches the repeated matrix inversion operation performed on 
+## same matrix. It stores the value returned by solve operation as a part
+## of the matrix itself and gives the cached result if the inverse operation
+## was performed before.
 
-## Write a short comment describing this function
+
+## Created the cache-matrix datastructure (list) that stores 
+##      1. Stores matrix data
+##      2. Has get() and set() function to get and set
+##              matrix data
+##      3. Has getInverse() and setInverse() functions
+##              to get and set inverse of the stored matrix
+## Argument : Input of class "matrix"
+## Output   : list with function handle to get and set matrix 
+##              and its inverse
+
 makeCacheMatrix <- function(x = matrix()){
         inv <- NULL
         set <- function(y){
@@ -22,17 +34,24 @@ makeCacheMatrix <- function(x = matrix()){
 }
 
 
-## Write a short comment describing this function
+## Function to find inverse of a matrix. 
+## Argument : matrix created using "makeCacheMatrix()" function
+## Output   : Inverse of the matrix
 cacheSolve <- function(x,...){
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getInverse()
+        ## Checks if the inverse was calculted before.
         if(!is.null(inv)){
+                ## Returns the cached result
                 message("Getting cached data...")
                 return(inv)
         }
-        
+        ## Retrieve the matrix from cached-matrix datastructure
         mat <- x$get()
+        ## Calculate the inverse
+        ## Will throw an error if the matrix in not a square matrix
         inv <- solve(mat)
+        ## Saves the result
         x$setInverse(inv)
         inv
 }
